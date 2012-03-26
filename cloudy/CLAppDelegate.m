cdc30e8a3657a8464f8dde836e4ffbeb899840d6
Evadne Wu <ev@monoceroi.com>
2012-03-27 02:19:07 +0800

    scaffolds push notification handling, adds Parse framework, fixes up model

--- cloudy/CLAppDelegate.m
+++ cloudy/CLAppDelegate.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+#import <Parse/Parse.h>
+
 #import "CLAppDelegate.h"
 
 @implementation CLAppDelegate
@@ -13,6 +15,10 @@
 
 - (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
+	[application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeSound];
+	
+	[Parse setApplicationId:@"aOcVK8sIpI6I2WksHgGaVluNyxf5pW4bsoHjdu7F" clientKey:@"CPTCmulUA8SaWeQoQc7ADoHBan1IN3HJnj9Kfey1"];
+	
 	if (!self.window)
 		self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	
@@ -23,6 +29,35 @@
 	
 	return YES;
 	
+}
+
+- (void) application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)newDeviceToken {
+
+	[PFPush storeDeviceToken:newDeviceToken];
+	[PFPush subscribeToChannelInBackground:@"" block: ^ (BOOL succeeded, NSError *error) {
+		
+		if (succeeded) {
+			NSLog(@"Successfully subscribed to the broadcast channel.");
+		} else {
+			NSLog(@"Failed to subscribe to the broadcast channel.");
+		}
+		
+	}];
+
+}
+
+- (void) application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+
+	NSLog(@"%s %@ %@", __PRETTY_FUNCTION__, application, error);
+
+}
+
+- (void) application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+
+	//	Heh
+
+	[PFPush handlePush:userInfo];
+
 }
 
 @end
